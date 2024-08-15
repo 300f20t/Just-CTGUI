441fa1e9c7f8f485b468f0ff4da08288e2dedc69
BuildTools <93136063+300f20t@users.noreply.github.com>
2024-08-15 07:46:17 +0300

    Update CtguicommandCommand.java

--- src/main/java/net/mcreator/justctgui/command/CtguicommandCommand.java
+++ src/main/java/net/mcreator/justctgui/command/CtguicommandCommand.java
@@ -20,7 +20,7 @@ import net.mcreator.justctgui.procedures.OpenFurnaceCTGUIProcedure;
 public class CtguicommandCommand {
 	@SubscribeEvent
 	public static void registerCommand(RegisterCommandsEvent event) {
-		event.getDispatcher().register(Commands.literal("ctgui").requires(s -> s.hasPermissionLevel(4)).then(Commands.literal("crafting_table").then(Commands.literal("create").executes(arguments -> {
+		event.getDispatcher().register(Commands.literal("ctgui").requires(s -> s.hasPermissionLevel(4)).then(Commands.literal("craftingTable").then(Commands.literal("addRecipe").executes(arguments -> {
 			World world = arguments.getSource().getWorld();
 			double x = arguments.getSource().getPos().getX();
 			double y = arguments.getSource().getPos().getY();
